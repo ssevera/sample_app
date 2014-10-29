5df5c4b765fbc67b60440c361637453f5f4c3b82
Stephanie Mayer <stephsmayer@gmail.com>
2014-10-29 23:42:41 +0000

    finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -50,6 +52,11 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+  
+    describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
   
 
